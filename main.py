8c01ba9e81435a8756e060d1a40be1b7068446fa
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2022-04-26 14:52:07 -0500

    up

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
+# python
+from lib2to3.pytree import Base
+from typing import Optional 
+
+#Pydantic
+from pydantic import BaseModel
+
+# fastAPI 
 from fastapi import FastAPI
+from fastapi import Body, Query
+
+
+#Models 
+class Person(BaseModel):
+    first_name: str 
+    last_name: str 
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 app = FastAPI()
 
@@ -10,5 +28,15 @@ def home():
 #Request and response Body
 
 @app.post("/person/new")
-def create_person():
-    pass 
+def create_person(person: Person = Body(...)):
+    return person
+
+# validaciones: Query Parameters 
+
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: str = Query(...)
+): 
+
+    return {name: age}
