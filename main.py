3dd8e586aff967f478a4054bad85ab5d9672bcf7
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2022-04-27 07:14:39 -0500

    Validaciones: Models

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # python
-from lib2to3.pytree import Base
 from typing import Optional 
+from enum import Enum
 
 #Pydantic
 from pydantic import BaseModel
+from pydantic import Field
 
 # fastAPI 
 from fastapi import FastAPI
@@ -12,17 +13,37 @@ from fastapi import Body, Query, Path
 
 #Models 
 
+class HairColor(Enum):
+    white = "white"
+    brown = "brown"
+    black = "black"
+    blonde = "blonde"
+    red = "red"
+
 class Location(BaseModel):
     city: str
     state: str
     country: str
 
 class Person(BaseModel):
-    first_name: str 
-    last_name: str 
-    age: int
-    hair_color: Optional[str] = None
-    is_married: Optional[bool] = None
+    first_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+        )
+    last_name: str = Field(
+        ...,
+        min_length=1,
+        max_length=50
+        )
+    age: int = Field(
+        ...,
+        gt=0,
+        le=105
+        )
+
+    hair_color: Optional[HairColor] = Field(default=None)
+    is_married: Optional[bool] = Field(default=None)
 
 app = FastAPI()
 
