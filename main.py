3c56cd0fc22fe2b971da1f88dcda19b31eb07193
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2022-04-29 06:52:52 -0500

    add .

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from pydantic import EmailStr
 # fastAPI 
 from fastapi import FastAPI
 from fastapi import status
-from fastapi import Body, Query, Path, Form, Header, Cookie
+from fastapi import Body, Query, Path, Form, Header, Cookie, UploadFile, File
 
 app = FastAPI()
 
@@ -166,3 +166,17 @@ def contact(
     ads: Optional[str] = Cookie(default=None)
 ):
     return user_agent
+
+# Files 
+
+@app.post(
+    path="/post-image"
+)
+def post_image(
+    image: UploadFile = File(...)
+):
+    return {
+        "Filename": image.filename,
+        "Format": image.content_type,
+        "Size(kb)": round(len(image.file.read())/1024, ndigits=2)
+    }
