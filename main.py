dce204d1c1e6f822836c6053eb93c4bc69282a23
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2022-04-29 06:09:28 -0500

    Cookie y Header Parameters

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from enum import Enum
 #Pydantic
 from pydantic import BaseModel
 from pydantic import Field
+from pydantic import EmailStr
 
 # fastAPI 
 from fastapi import FastAPI
 from fastapi import status
-from fastapi import Body, Query, Path, Form
+from fastapi import Body, Query, Path, Form, Header, Cookie
 
 app = FastAPI()
 
@@ -128,6 +129,9 @@ def update_person(
     # results = person.dict()
     # results.update(Location.dict())
     return person
+
+# Forms
+
 @app.post(
     path="/login",
     response_model=LoginOut,
@@ -135,3 +139,30 @@ def update_person(
 )
 def login(username: str = Form(...), password: str = Form(...)):
     return LoginOut(username=username)
+
+# Cookies and Headers Parameters 
+
+@app.post(
+    path="/contact",
+    status_code=status.HTTP_200_OK
+)
+def contact(
+    first_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=1
+    ),
+    last_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=1
+    ),
+    email: EmailStr = Form(...),
+    message: str = Form(
+        ...,
+        min_length=20
+    ),
+    user_agent: Optional[str] = Header(default=None),
+    ads: Optional[str] = Cookie(default=None)
+):
+    return user_agent
